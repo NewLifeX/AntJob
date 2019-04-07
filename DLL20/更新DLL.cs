eed7a7619fb2dede8d3b6ae0fe9c15907dbbecda
大石头 <nnhy@vip.qq.com>
2019-04-07 09:53:56 +0800

    更新基础组件，改为从nuget引用

--- DLL20/更新DLL.cs
+++
@@ -1,4 +0,0 @@
-// 自动选择最新的文件源
-var di = ".".AsDirectory();
-var srcs = new String[] { @"..\Bin\netstandard2.0", @"..\..\Bin\netstandard2.0", @"..\..\..\X\Bin\netstandard2.0" };
-di.CopyIfNewer(srcs, "*.dll;*.exe;*.xml;*.pdb;*.cs");
