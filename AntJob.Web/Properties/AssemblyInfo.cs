d44f51377a502fa4cb8ae52d9ce01b7727afd186
大石头 <nnhy@vip.qq.com>
2019-09-08 12:12:16 +0800

    升级X组件到0822版

--- AntJob.Web/Properties/AssemblyInfo.cs
+++ AntJob.Web/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用“修订号”和“内部版本号”的默认值，
 // 方法是按如下所示使用 "*":
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.2019.0409")]
+[assembly: AssemblyFileVersion("1.0.2019.0908")]
