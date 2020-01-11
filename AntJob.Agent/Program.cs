9c0e885ef68ec976b3ecb05efca5ced00769b135
大石头 <nnhy@vip.qq.com>
2020-01-11 09:48:44 +0800

    新增蚂蚁调度代理 AntJob.Agent，调度中心下发C#或Sql给蚂蚁代理执行

---
+++ AntJob.Agent/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using AntJob.Extensions;
+using AntJob.Handlers;
+using AntJob.Providers;
+using NewLife;
+using NewLife.Agent;
+using NewLife.Remoting;
+using NewLife.Threading;
+
+namespace AntJob.Agent
+{
+    class Program
+    {
+        static void Main(String[] args) => new MyService().Main();
+    }
+
+    /// <summary>服务类。名字可以自定义</summary>
+    class MyService : AgentServiceBase<MyService>
+    {
+        public MyService()
+        {
+            ServiceName = "AntAgent";
+
+            // 注册菜单，在控制台菜单中按 t 可以执行Test函数，主要用于临时处理数据
+            AddMenu('t', "数据测试", Test);
+        }
+
+        private Scheduler _Scheduler;
+        /// <summary>服务启动</summary>
+        /// <remarks>
+        /// 安装Windows服务后，服务启动会执行一次该方法。
+        /// 控制台菜单按5进入循环调试也会执行该方法。
+        /// </remarks>
+        protected override void StartWork(String reason)
+        {
+            var set = Setting.Current;
+
+            // 实例化调度器
+            var sc = new Scheduler();
+
+            // 使用分布式调度引擎替换默认的本地文件调度
+            sc.Provider = new NetworkJobProvider
+            {
+                Server = set.Server,
+                AppID = set.AppID,
+                Secret = set.Secret,
+            };
+
+            // 添加作业处理器
+            sc.Jobs.Add(new CSharpHandler());
+            sc.Jobs.Add(new SqlHandler());
+
+            // 启动调度引擎，调度器内部多线程处理
+            sc.Start();
+
+            _Scheduler = sc;
+
+            base.StartWork(reason);
+        }
+
+        /// <summary>服务停止</summary>
+        /// <remarks>
+        /// 安装Windows服务后，服务停止会执行该方法。
+        /// 控制台菜单按5进入循环调试，任意键结束时也会执行该方法。
+        /// </remarks>
+        protected override void StopWork(String reason)
+        {
+            base.StopWork(reason);
+
+            _Scheduler.TryDispose();
+            _Scheduler = null;
+        }
+
+        /// <summary>数据测试，菜单t，默认文件调度</summary>
+        public void Test()
+        {
+        }
+    }
+}
---
+++ AntJob.Agent/Setting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using NewLife.Xml;
+
+namespace AntJob.Agent
+{
+    /// <summary>配置</summary>
+    [XmlConfigFile("Config/AntAgent.config", 15000)]
+    public class Setting : XmlConfig<Setting>
+    {
+        #region 属性
+        /// <summary>调试开关。默认true</summary>
+        [Description("调试开关。默认true")]
+        public Boolean Debug { get; set; } = true;
+
+        /// <summary>调度中心。逗号分隔多地址，主备架构</summary>
+        [Description("调度中心。逗号分隔多地址，主备架构")]
+        public String Server { get; set; } = "tcp://127.0.0.1:9999,tcp://ant.newlifex.com:9999";
+
+        /// <summary>应用标识。调度中心以此隔离应用，默认AntAgent</summary>
+        [Description("应用标识。调度中心以此隔离应用，默认AntAgent")]
+        public String AppID { get; set; } = "AntAgent";
+
+        /// <summary>应用密钥。</summary>
+        [Description("应用密钥。")]
+        public String Secret { get; set; }
+        #endregion
+    }
+}
