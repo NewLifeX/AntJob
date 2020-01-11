bfaccc66a1cfcb783200665da63ce54d4113c85e
大石头 <nnhy@vip.qq.com>
2020-01-11 11:38:15 +0800

    AntJob优先支持分布式网络调度，提升配置文件优先级

--- AntJob/Setting.cs
+++ AntJob/Setting.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Reflection;
 using NewLife.Xml;
 
-namespace Stardust
+namespace AntJob
 {
-    /// <summary>配置</summary>
-    [XmlConfigFile("Config/Stardust.config", 15000)]
+    /// <summary>蚂蚁配置。主要用于网络型调度系统</summary>
+    [XmlConfigFile("Config/Ant.config", 15000)]
     public class Setting : XmlConfig<Setting>
     {
         #region 属性
@@ -13,15 +14,30 @@ namespace Stardust
         [Description("调试开关。默认true")]
         public Boolean Debug { get; set; } = true;
 
-        /// <summary>服务端口。默认6666</summary>
-        [Description("服务端口。默认6666")]
-        public Int32 Port { get; set; } = 6666;
+        /// <summary>调度中心。逗号分隔多地址，主备架构</summary>
+        [Description("调度中心。逗号分隔多地址，主备架构")]
+        public String Server { get; set; } = "tcp://127.0.0.1:9999,tcp://ant.newlifex.com:9999";
+
+        /// <summary>应用标识。调度中心以此隔离应用，默认当前应用</summary>
+        [Description("应用标识。调度中心以此隔离应用，默认当前应用")]
+        public String AppID { get; set; }
+
+        /// <summary>应用密钥。</summary>
+        [Description("应用密钥。")]
+        public String Secret { get; set; }
         #endregion
 
-        #region 构造
-        /// <summary>实例化</summary>
-        public Setting()
+        #region 方法
+        /// <summary>重载</summary>
+        protected override void OnLoaded()
         {
+            if (AppID.IsNullOrEmpty())
+            {
+                var asm = Assembly.GetEntryAssembly();
+                if (asm != null) AppID = asm.GetName().Name;
+            }
+
+            base.OnLoaded();
         }
         #endregion
     }
--- Samples/HelloWork/Program.cs
+++ Samples/HelloWork/Program.cs
@@ -11,11 +11,18 @@ namespace HelloWork
         {
             XTrace.UseConsole();
 
+            var set = Setting.Current;
+
             // 实例化调度器
             var sc = new Scheduler();
 
             // 使用分布式调度引擎替换默认的本地文件调度
-            sc.Provider = new NetworkJobProvider { Server = "tcp://127.0.0.1:9999,tcp://ant.newlifex.com:9999", AppID = "Hello" };
+            sc.Provider = new NetworkJobProvider
+            {
+                Server = set.Server,
+                AppID = set.AppID,
+                Secret = set.Secret,
+            };
 
             // 添加作业处理器
             sc.Jobs.Add(new HelloJob());
