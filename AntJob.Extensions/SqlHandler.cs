31eba921f477dfda9b2ed9f571909085c668fbcf
大石头 <nnhy@vip.qq.com>
2020-04-12 00:12:00 +0800

    SQL处理器成功解析sql语句，并执行后返回

--- AntJob.Extensions/SqlHandler.cs
+++ AntJob.Extensions/SqlHandler.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AntJob.Data;
+using XCode.DataAccessLayer;
 
 namespace AntJob.Extensions
 {
@@ -29,10 +31,23 @@ namespace AntJob.Extensions
         /// <returns></returns>
         protected override Int32 Execute(JobContext ctx)
         {
-            var sql = ctx.Data as String;
+            var sql = ctx.Task.Data as String;
             if (sql.IsNullOrWhiteSpace()) return -1;
 
-            return 0;
+            // 解析出来连接名
+            var connName = "";
+            var str = sql.Substring("/*", "*/")?.Trim();
+            if (!str.IsNullOrEmpty()) connName = str.Substring("use ")?.Trim();
+            if (connName.IsNullOrEmpty()) throw new InvalidDataException("无法解析连接名");
+
+            // 剩下的Sql
+            sql = sql.Substring("*/")?.Trim();
+            if (sql.IsNullOrWhiteSpace()) return -1;
+
+            var dal = DAL.Create(connName);
+            var rs = dal.Execute(sql);
+
+            return rs;
         }
     }
 }
