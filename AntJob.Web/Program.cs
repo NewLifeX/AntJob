62730946b362db2b8c45c8d906356424194cab8c
智能大石头 <nnhy@vip.qq.com>
2022-12-22 23:41:53 +0800

    打开日志，web段默认端口9980

--- AntJob.Web/Program.cs
+++ AntJob.Web/Program.cs
@@ -4,30 +4,29 @@ using Microsoft.Extensions.Hosting;
 using NewLife.Cube;
 using NewLife.Log;
 
-namespace AntJob.Web
+namespace AntJob.Web;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            XTrace.UseConsole();
+        XTrace.UseConsole();
 
-            var app = ApplicationManager.Load();
-            do
+        var app = ApplicationManager.Load();
+        do
+        {
+            app.Start(CreateHostBuilder(args).Build());
+        } while (app.Restarting);
+    }
+
+    public static IHostBuilder CreateHostBuilder(String[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(logging => { logging.AddXLog(); })
+            .ConfigureWebHostDefaults(webBuilder =>
             {
-                app.Start(CreateHostBuilder(args).Build());
-            } while (app.Restarting);
-        }
-
-        public static IHostBuilder CreateHostBuilder(String[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging => { logging.AddXLog(); })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    //webBuilder.UseUrls("http://*:5000;https://*:5001");
-                    webBuilder.UseStartup<Startup>();
-                });
-        }
+                //webBuilder.UseUrls("http://*:5000;https://*:5001");
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
