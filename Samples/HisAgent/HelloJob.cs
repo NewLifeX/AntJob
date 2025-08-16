f02a6cf5c79c8ac9f3497e0698d086e43c8d41dd
大石头 <nnhy@vip.qq.com>
2025-08-17 00:25:48 +0800

    重写项目说明

--- Samples/HisAgent/HelloJob.cs
+++ Samples/HisAgent/HelloJob.cs
@@ -17,10 +17,7 @@ namespace HisAgent;
 [Description("简单的定时任务")]
 internal class HelloJob : Handler
 {
-    public HelloJob()
-    {
-        Job.Cron = "7/30 * * * * ?";
-    }
+    public HelloJob() => Job.Cron = "7/30 * * * * ?";
 
     public override Int32 Execute(JobContext ctx)
     {
@@ -30,9 +27,6 @@ internal class HelloJob : Handler
         var time = ctx.Task.DataTime;
         WriteLog("新生命蚂蚁调度系统！当前任务时间：{0}", time);
         if (!ctx.Task.Data.IsNullOrEmpty()) WriteLog("数据：{0}", ctx.Task.Data);
-
-        //// 一定几率抛出异常
-        //if (Rand.Next(2) == 0) throw new Exception("Error");
 
         // 成功处理数据量
         return 1;
